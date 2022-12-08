2db41f215575866b999e38544b1b8532bffa618c
bowen-wu <13793248162@163.com>
2022-12-08 09:06:14 +0800

    feat: init

---
+++ src/layouts/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'react-vant';
+
+export default function Layout() {
+    return (
+        <div>
+            <Button round nativeType="submit" type="primary" block>
+                提交
+            </Button>
+        </div>
+    );
+}
